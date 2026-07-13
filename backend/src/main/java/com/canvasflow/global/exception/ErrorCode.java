6ed8839612766ce1381e86a5af833fdb0e3789a3
Anemone_sea <lheek00@gmail.com>
2026-07-14 01:25:56 +0900

    2026.07.14 이희경 ErrorCode 수정

--- backend/src/main/java/com/canvasflow/global/exception/ErrorCode.java
+++ backend/src/main/java/com/canvasflow/global/exception/ErrorCode.java
@@ -37,6 +37,10 @@ public enum ErrorCode {
     SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "SUB_001", "구독 정보를 찾을 수 없습니다."),
     ALREADY_SUBSCRIBED(HttpStatus.CONFLICT, "SUB_002", "이미 구독 중입니다."),
     SUBSCRIPTION_REQUIRED(HttpStatus.FORBIDDEN, "SUB_003", "구독이 필요한 콘텐츠입니다."),
+    TIER_NOT_FOUND(HttpStatus.NOT_FOUND, "SUB_004", "존재하지 않는 구독 등급입니다."),
+    TIER_NOT_IN_CHANNEL(HttpStatus.BAD_REQUEST, "SUB_005", "해당 채널의 구독 등급이 아닙니다."),
+    TIER_LEVEL_DUPLICATED(HttpStatus.CONFLICT, "SUB_006", "이미 존재하는 등급 레벨입니다."),
+    TIER_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "SUB_007", "등급은 최대 5개까지 만들 수 있습니다."),
 
     // Comment / Like
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT_001", "댓글을 찾을 수 없습니다."),
